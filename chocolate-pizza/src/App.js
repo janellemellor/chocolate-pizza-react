a5adb37e139e2150c81560bbf9c5daedda7d8d87
janelle mellor <janellemellor@gmail.com>
2020-02-10 17:39:59 -0800

    add footer and sectionOne

--- chocolate-pizza/src/App.js
+++ chocolate-pizza/src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from "./Header.js";
+import Footer from "./Footer.js";
+import SectionOne from "./SectionOne.js";
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
       <Header /> 
+      <SectionOne />
+      <Footer />
       </div>
     );
   }
---
+++ chocolate-pizza/src/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div> 
+                <p>Delicious &copy; 2013 . All Rights Reserved.
+                Proudly published with Ghost.</p>  
+            </div>
+        );
+    }
+}
