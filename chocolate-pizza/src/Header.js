3656a47fc84e3b89372b2c5f48c00b6fc21edc58
janelle mellor <janellemellor@gmail.com>
2020-02-10 17:01:51 -0800

    add header

---
+++ chocolate-pizza/src/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Header extends Component {
+    render() {
+        return (
+        <div>
+            <h2>Delicious</h2>
+                <span>
+                    The best food blog on the web</span>
+        </div>    
+        );
+    }
+}
